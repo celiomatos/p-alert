436cddd4648129de3f9eccfe60f85c31fbe4a5c3
Celio Matos <celiomatos@live.com>
2019-06-26 22:20:37 -0400

    sjsjfkjfk

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ const page = await browser.newPage();
 await page.setViewport({width: 1280, height: 800});
 
 await page.goto(
-`https://www.google.com/search?rlz=1C1GCEU_pt-BRBR821BR821&ei=ac72XKqwJcKc5wKxhLuIDQ&q=
-spring+boot&oq=spring+boot&gs_l=psy-ab.3..0l10.653195.654967..655961...0.0..0.344.2774.2-10j1
-......0....1..gws-wiz.......0i131j0i67j0i10.1ZjkHiTxEeM`);
+`https://www.google.com.br/search?q=%22dermilson+chagas%22&lr=&hl=pt-BR&source=lnt&tbs=sbd:1,qdr:h&sa=X&ved=0ahUKEwiL-PiprP7aAhWCIJAKHXUyDXwQpwUIHw&biw=1360&bih=637`);
 
 const data = await page.$$eval('#search a', tds => tds.map((td) => {
   return td.href;
@@ -30,12 +28,12 @@ var sites = "Links\n";
 for(let i = 0; i < data.length; i++){
 	const noticia = await data[i];		
 	try {
-		if(!(noticia.includes("google.com") || noticia.includes("youtube.com") || noticia.includes("webcache.googleusercontent.com"))){	  
+		if(!(noticia.includes("google.com") || noticia.includes("youtube.com") || noticia.includes("webcache.googleusercontent.com") || noticia.includes("www.portaldogeneroso"))){	  
 			await page.goto(noticia);
 			console.log(">>>> " + noticia);
 	        const title = await page.title();
 			const content = await page.content();
-			if(!content.includes("2013-2019")){
+			if(content.toLowerCase().includes("dermilson")){
                 await page.screenshot({ path: folder + '/' + title + '.png', fullPage: true });
 		        sites += noticia + "\n";
 			}
